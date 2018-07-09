8ceafca9a220c4c62fda3369c684701c9478fab8
thejus manoharan <thejus.manoharan@gmail.com>
2018-07-09 11:31:06 +0200

    updated stored procedures

--- Stored Procedures/storedProcedures.sql
+++ Stored Procedures/storedProcedures.sql
@@ -27,25 +27,28 @@ use DELIMITER $$
 		SELECT tmp_date, tmp_date + INTERVAL 1 MINUTE;
 
 		INSERT INTO user(user_id,email,pwd,name,first_name,token,created_at,validated_at) VALUES
-		(1, 'haddock@moulinsart.be', 'capitaine', 'Haddock', 'Archibald', 'xys123asd_ad1', '2018-09-16 00:00:00', '2018-09-17 00:00:00'),
-		(2, 'trainer@moulinsart.be', 'capitaine2', 'Trainer', 'Stephen', 'xys123asd_ad12', '2018-09-18 00:00:00', '2018-09-19 00:00:00'),
-		(3, 'teacher@moulinsart.be', 'capitaine3', 'Trainer', 'Mohit', 'asd_1we88s', '2018-09-19 16:00:00', '2018-09-20 17:00:00'),
-		(4, 'thejus@thejus.fr', 'thejus123', 'thejus', 'THEJUS', 'adasd*7687A_asd', '2017-09-19 00:20:00', '2017-09-20 01:00:00'),
-		(5, 'nongroupuser@nogroup.fr', 'test123', 'Test', 'Test', 'asdasd_asdqw123*(*', '2017-08-08 10:00:00', '2017-08-08 11:00:00');
+		(1, 'haddock@moulinsart.be', 'capitaine', 'Haddock', 'Archibald', 'xys123asd_ad1', tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR),
+		(2, 'trainer@moulinsart.be', 'capitaine2', 'Trainer', 'Stephen', 'xys123asd_ad12', tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR),
+		(3, 'teacher@moulinsart.be', 'capitaine3', 'Trainer', 'Mohit', 'asd_1we88s',tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR),
+		(4, 'thejus@thejus.fr', 'thejus123', 'thejus', 'THEJUS', 'adasd*7687A_asd', tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR),
+		(5, 'nongroupuser@nogroup.fr', 'test123', 'Test', 'Test', 'asdasd_asdqw123', tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR),
+        (6, 'test@test.be', 'testCaptain', 'Master', 'TEST', 'thisasd!asdsa_22',tmp_date - INTERVAL 2 DAY, tmp_date - INTERVAL 2 DAY + INTERVAL 1 HOUR);
 
 		INSERT INTO trainer(user_id) VALUES
 		(2),
 		(3);
 
 		INSERT INTO usergroup(group_id,name,trainer_id,created_at) VALUES
-		(1, 'SE', 2 , '2018-09-17 00:00:00'),
-		(2, 'ISM', 2 , '2018-09-27 00:00:00'),
-		(3, 'DS', 3 , '2018-03-17 00:00:00');
+		(1, 'SE', 2 , tmp_date - INTERVAL 2 DAY),
+		(2, 'ISM', 2 ,tmp_date - INTERVAL 2 DAY),
+		(3, 'DS', 3 , tmp_date - INTERVAL 2 DAY);
 
 		INSERT INTO group_member(user_id,group_id,validated_at) VALUES
-		(1,1,'2018-09-17 00:00:00'),
-		(3,3,'2017-09-20 00:00:00'),
-		(5,2,'2017-09-20 00:00:00');
+		(1,1,tmp_date - INTERVAL 2 DAY + INTERVAL 2 HOUR),
+        (6,1,tmp_date - INTERVAL 2 DAY + INTERVAL 2 HOUR),
+		(3,3,tmp_date - INTERVAL 2 DAY + INTERVAL 2 HOUR),
+		(5,2,tmp_date - INTERVAL 2 DAY + INTERVAL 2 HOUR);
+        
 
 
 		INSERT INTO theme(theme_id,label) VALUES
@@ -62,16 +65,17 @@ use DELIMITER $$
 
 		INSERT INTO sql_question(question_id,db_name,question_text,correct_answer,correct_result,theme_id,author_id,is_public) VALUES
 		(1,'auction','Perform select opertaion on table bid','SELECT * from bid','(1,4,17.0,2018-09-19 00:00:00),(2,3,9.0,2018-07-10 00:00:00)',1,2,'True'),
-		(2,'auction','Perform delete opertaion on table row where id = 1','DELETE  from bid where id = 1','1 Row modified',1,3,'True');
+	    (2,'auction','Perform delete opertaion on table row where id = 1','DELETE  from bid where id = 1','1 Row modified',1,3,'True'),
+        (3,'auction','Perform delete opertaion on table row where id = 1','DELETE  from bid where id = 1','1 Row modified',1,3,'True');
 
 		INSERT INTO sql_quiz_question(question_id,quiz_id,rank) VALUES
 		(1,1,5),
-		(2,2,10);
+		(2,1,10);
 
 
 		INSERT INTO evaluation(evaluation_id,group_id,trainer_id,quiz_id,scheduled_at,ending_at,completed_at) VALUES
-		(1,1,2,1,tmp_date,tmp_date + INTERVAL 1 MINUTE,'2018-10-01 18:20:00'),
-		(2,2,3,2,tmp_date,tmp_date + INTERVAL 1 MINUTE,'2018-10-01 17:20:00');
+		(1,1,2,1,tmp_date,tmp_date + INTERVAL 5 MINUTE,null),
+		(2,2,3,2,tmp_date - INTERVAL 1 DAY,tmp_date - INTERVAL 1 DAY + INTERVAL 1 HOUR, tmp_date);
 
 		INSERT INTO sheet(trainee_id,evaluation_id,started_at,completed_at,validated_at) VALUES
 		(1,1,'2018-10-01 18:02:00','2018-10-01 18:20:00','2018-10-01 19:00:00'),
