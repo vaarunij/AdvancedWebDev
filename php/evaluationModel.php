57b5c03ae775f62975e4e9d34296ed75b7185f92
Chris LeMatty <clematty@gmail.com>
2018-07-10 04:23:33 +0200

    Fixing conflicting filenames

--- php/evaluationModel.php
+++ php/evaluationModel.php
@@ -1,19 +1,36 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-require_once("SqlSkillsDB.php");
-session_start();
-
-/** Access to the person table.
- * Put here the methods like getBySomeCriteriaSEarch */
-class evaluationModel {
+require_once 'SqlSkillsDB.php';
+class EvaluationModel {
      public static function getEvaluation() {
         $db = SqlSkillsDB::getConnection();
-        $sql = "SELECT evaluation_id
-              FROM evaluation WHERE trainer";              
+        $sql = "SELECT * FROM evaluation";
         $stmt = $db->prepare($sql);
         $ok = $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
-
+    public static function postEvaluation() {
+        $db = SqlSkillsDB::getConnection();
+        $evaluation_id = $_POST['evaluation_id'];
+        $group_id = $_POST['group_id'];
+        $trainer_id = $_POST['trainer_id'];
+        $quiz_id = $_POST['quiz_id'];
+        $scheduled_at = date('Y-m-d H:i:s');
+        $ending_at = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO evaluation(evaluation_id, group_id, trainer_id, quiz_id, scheduled_at, ending_at)
+            VALUES (:evaluation_id, :group_id, :trainer_id, :quiz_id, :scheduled_at, :ending_at)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":evaluation_id", $evaluation_id);
+        $stmt->bindValue(":group_id", $group_id);
+        $stmt->bindValue(":trainer_id", $trainer_id);
+        $stmt->bindValue(":quiz_id", $quiz_id);
+        $stmt->bindValue(":scheduled_at", $scheduled_at);
+        $stmt->bindValue(":ending_at", $ending_at);
+        $ok = $stmt->execute();
+        echo "string";
+        echo $ok;
+        // return $ok;
+        // return $stmt->fetch(PDO::FETCH_ASSOC);
+        // getQuizzes();
+    }
+}
 ?>
